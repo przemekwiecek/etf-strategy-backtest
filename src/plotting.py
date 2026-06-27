ae817e5b873e0a4be21d8f05a47b31b64f4b3c2a
przemekwiecek <velvox@icloud.com>
2026-06-27 22:00:12 +0200

    Reduced redundancy and made the code more clear

--- src/plotting.py
+++ src/plotting.py
@@ -1,53 +1,49 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 
-def build_plots(df, strat, nominal, real):
-    plt.style.use('ggplot')
+def build_ax_fig(title: str) -> tuple[plt.Figure, plt.Axes]:
     fig, ax = plt.subplots()
+    ax.set_title(title)
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Value")
+    fig.set_size_inches([10, 5])
+    ax.set_xlim(pd.Timestamp("2020-01-01"), pd.Timestamp("2025-12-01"))
+
+    return fig, ax
+
+def build_plots(df: pd.DataFrame, strat: str, nominal: float, real: float):
+    plt.style.use('ggplot')
+    fig, ax = build_ax_fig("Portfolio Value vs Money Invested")
     ax.plot(df["date"], df["money_invested_total"], label = "Money Invested")
     ax.plot(df["date"], df["portfolio_value"], label = "Portfolio Value")
     ax.plot(df["date"], df["real_portfolio_value"], label = "Real Portfolio Value")
     if strat == "dca":
-        ax.text(pd.Timestamp("2025-02-01"), 1000, f"XIRR: {nominal}%")
-        ax.text(pd.Timestamp("2025-02-01"), 500, f"Real XIRR: {real}%")
+        ax.text(pd.Timestamp("2024-12-01"), 1000, f"XIRR: {nominal}%")
+        ax.text(pd.Timestamp("2024-12-01"), 500, f"Real XIRR: {real}%")
     elif strat == "lump":
         ax.text(pd.Timestamp("2024-12-01"), 6500, f"CAGR: {nominal}%")
         ax.text(pd.Timestamp("2024-12-01"), 6000, f"Real CAGR: {real}%")
     else:
         raise ValueError("Wrong value")
-    ax.set_title("Portfolio Value vs Money Invested")
     ax.legend()
-    fig.set_size_inches([10, 5])
-    ax.set_xlim(pd.Timestamp("2020-01-01"), pd.Timestamp("2025-12-01"))
     plt.tight_layout()
 
     if strat != "lump":
-        plt.style.use('ggplot')
-        fig, ax = plt.subplots()
+        fig, ax = build_ax_fig("Profit over time")
         ax.plot(df["date"], df["profit"], label = "Profit")
         ax.set_title("Profit over time")
         ax.legend()
-        fig.set_size_inches([10, 5])
-        ax.set_xlim(pd.Timestamp("2020-01-01"), pd.Timestamp("2025-12-01"))
         plt.tight_layout()
 
     if strat != "lump":
-        plt.style.use('ggplot')
-        fig, ax = plt.subplots()
+        fig, ax = build_ax_fig("Total number of units over time")
         ax.plot(df["date"], df["units_total"], label="Total Units")
-        ax.set_title("Total number of units over time")
         ax.legend()
-        fig.set_size_inches([10, 5])
-        ax.set_xlim(pd.Timestamp("2020-01-01"), pd.Timestamp("2025-12-01"))
         plt.tight_layout()
 
-    plt.style.use('ggplot')
-    fig, ax = plt.subplots()
+    fig, ax = build_ax_fig("VUAA.L ETF Value")
     ax.plot(df["date"], df["close"], label="Closing Price")
-    ax.set_title("VUAA.L ETF Value")
     ax.legend()
-    fig.set_size_inches([10, 5])
-    ax.set_xlim(pd.Timestamp("2020-01-01"), pd.Timestamp("2025-12-01"))
     plt.tight_layout()
 
     plt.show()
